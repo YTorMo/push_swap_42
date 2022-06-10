f088639cff7c6493c624dbba65bdaf3246c23ead
Yago Toro molina <ytoro-mo@c1r9s1.42malaga.com>
2022-06-10 08:22:08 +0200

    Push_Swap1

--- src/push_swap_four_elem.c
+++ src/push_swap_four_elem.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap_five_elem.c                              :+:      :+:    :+:   */
+/*   push_swap_four_elem.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ytoro-mo <ytoro-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/08 15:22:06 by ytoro-mo          #+#    #+#             */
-/*   Updated: 2022/06/10 08:21:14 by ytoro-mo         ###   ########.fr       */
+/*   Created: 2022/06/08 16:12:12 by ytoro-mo          #+#    #+#             */
+/*   Updated: 2022/06/08 16:23:38 by ytoro-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,14 @@
 void	ft_sort_five(t_stk **a, int max_size)
 {
 	t_stk	**b;
-	int		i;
+	t_stk	*memo;
 
 	b = malloc (sizeof(t_stk));
 	memo = malloc (sizeof(t_stk));
-	i = 0;
 	if (b && memo)
 	{
-		*b = NULL;
-		i = ft_lookfor_pos(a, 0);
-		ft_lookfor_and_sort(a, b, i, max_size);
+		memo
 		free(memo);
 		free(b);
 	}
-}
-
-void	ft_lookfor_and_sort(t_stk **a, t_stk **b, int i, int max_size)
-{
-	if (i == 0)
-		ft_push_ab(a, b, max_size, 'b');
-	else if (i <= 2)
-	{
-		while ((*a)->rlo_pos !=0)
-	}
-}
-
-int	ft_lookfor_pos(t_stk **a, int pos)
-{
-	t_stk	*memo;
-	int		i;
-
-	i = 0;
-	memo = *a;
-	while (memo->rl_pos != pos)
-	{
-		i++;
-		memo = memo->nxt;
-	}
-	return (i);
 }
